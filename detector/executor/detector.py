bd01bbe3c056c400bfc7ad89f1d93b111e774e41
OrionSolaris <osirishyp@gmail.com>
2023-04-03 19:14:31 +0800

    [update] use ultralytics YOLOv8 onnx and pytorch versions as detector

--- detector/executor/detector.py
+++ detector/executor/detector.py
@@ -2,44 +2,29 @@ from jina import Executor, requests, DocumentArray
 from rich import print
 
 from ultralytics import YOLO
-from ultralytics.yolo.engine.results import Boxes
-from ultralytics.yolo.utils.ops import non_max_suppression
-import cv2
+from ultralytics.yolo.utils import ROOT, yaml_load
+from ultralytics.yolo.utils.checks import check_yaml
+import torch
 
-frame = cv2.imread("../horses.jpeg")
-model = YOLO("./weights/yolov8n")
-results = model.predict(frame, classes=[0, 1])
 
-# print(type(results[0].boxes))
-# print(dir(results[0].boxes))
+class Detector(Executor):
+    def __init__(
+        self,
+        weights: str,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.model = YOLO(weights, task="detect")
+        self.classes = yaml_load(check_yaml("coco128.yaml"))["names"]
+        self.device = "0" if torch.cuda.is_available() else "cpu"
 
-# for x in results[0].boxes:
-#     print(x)
-# print(results[0].boxes.boxes)
-# boxes = non_max_suppression(results[0].boxes.boxes)
-# print(model.overrides)
-res_plotted = results[0].plot(line_width=1)
-cv2.imshow("1", res_plotted)
-if cv2.waitKey(0) == ord("q"):
-    raise StopIteration
-# class Detector(Executor):
-#     def __init__(
-#         self,
-#         weights: str,
-#         *args,
-#         **kwargs,
-#     ):
-#         Executor.__init__(self, *args, **kwargs)
-
-#         self.model  = YOLO(weights)
-
-#     @requests
-#     def detect(self, docs: DocumentArray, **kwargs):
-#         docs_to_return = DocumentArray()
-#         for doc in docs:
-#             results = self.model(doc.tensor)
-#             print(type(results[0].boxes))
-#             for x in results[0].boxes:
-#                 print(x)
-
-#             res_plotted = results[0].plot(line_width=1)
+    @requests
+    def detect(self, docs: DocumentArray, **kwargs):
+        docs_to_return = DocumentArray()
+        for doc in docs:
+            results = self.model.predict(
+                doc.tensor, classes=doc.tags.get("classes"), device=self.device
+            )
+            doc.tensor = results[0].plot(line_width=2)
+            docs_to_return.append(doc)
+        return docs_to_return
